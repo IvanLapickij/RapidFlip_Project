af0f2f5522eb9f2b3c736e43c206dfc4ca286b73
Ivan Lapickij <ivan.lapickij@gmail.com>
2023-10-12 21:06:51 +0100

    added about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,20 +6,22 @@ import { Fade } from "react-awesome-reveal";
 const About: React.FC = () => {
   return (
     <div className="container marketing">
-      <h1 id="About">About Us</h1>
+      <h1 id="About">Video Games and Mental Health</h1>
       {/* START THE FEATURETTES */}
       <hr className="featurette-divider" />
       <Fade delay={1e3} cascade damping={1e-1}>
         <div className="row featurette">
           <div className="col-md-7">
-            <h2 className="featurette-heading fw-normal lh-1">
-              Welcome to Rapid Flip{" "}
-            </h2>
+            <h2 className="featurette-heading fw-normal lh-1"> </h2>
             <p className="lead">
-              In our game, players become fearless pilots navigating advanced
-              aircraft in a high-stakes alternate reality. With stunning visuals
-              and immersive gameplay, prepare for thrilling aerial missions,
-              dogfights, and a captivating storyline..
+              Many young people play video games, but concerns about their
+              impact on stress and mental health exist. Encouraging mindful game
+              choices, awareness, and support for mental health is essential,
+              and
+              <span className="text-warning">
+                {" "}
+                ongoing research can help guide responsible gaming habits.
+              </span>
             </p>
           </div>
           <div className="col-md-5">
@@ -32,14 +34,16 @@ const About: React.FC = () => {
         <div className="row featurette">
           <div className="col-md-7 order-md-2">
             <h2 className="featurette-heading fw-normal lh-1">
-              Discover the Charms of Ireland{" "}
-              <span className="text-body-secondary">Versitile</span>
+              Our Sollution{" "}
+              {/* <span className="text-body-secondary">Versitile</span> */}
             </h2>
             <p className="lead">
-              Customize your aircraft and tackle missions in diverse landscapes,
-              forging alliances, uncovering secrets, and confronting formidable
-              foes. Your choices shape the narrative as you hone your piloting
-              skills in various scenarios.
+              We invite psychologists, elementary school teachers, and game
+              lovers to join our team, and interview gamers to explore effective
+              game mechanics that combine learning with enjoyment,{" "}
+              <span className="text-warning">
+                to bring positive change to the gaming community.
+              </span>
             </p>
           </div>
           <div className="col-md-5 order-md-1">
@@ -52,14 +56,14 @@ const About: React.FC = () => {
         <div className="row featurette">
           <div className="col-md-7">
             <h2 className="featurette-heading fw-normal lh-1">
-              Treasures Await{" "}
+              To make difference{" "}
             </h2>
             <p className="lead">
-              Beyond the cockpit, explore a dynamic world, interact with ground
-              crews, and witness the impact of your actions. The game offers
-              dramatic moments, camaraderie, and personal growth as you
-              transform from a novice pilot to a legendary ace. It's an
-              unforgettable journey of heroism in the skies.
+              There are very few developers working on games mixing
+              entertainment with education. And{" "}
+              <span className="text-warning">
+                we found it’s a great opportunity to work on them in Ireland.
+              </span>
             </p>
           </div>
           <div className="col-md-5">
--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import videoBg from '../assets/Animation.mp4';
+import React from "react";
+import videoBg from "../assets/Animation.mp4";
+// import Navbar from "./Navbar";
 
 const Main: React.FC = () => {
   return (
-    <div className='main'>
+    <div className="main">
       <div className="overlay"></div>
       <video src={videoBg} autoPlay loop muted />
       <div className="content">
         <h1>Welcome</h1>
-        <p>To my site.</p>
+        <h4>Rapid Flip Project.</h4>
       </div>
     </div>
   );
-}
+};
 
 export default Main;
