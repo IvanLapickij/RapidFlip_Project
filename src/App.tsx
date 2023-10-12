e30b8a2fd24f2fd56c39026d751bf6299819a303
Ivan Lapickij <ivan.lapickij@gmail.com>
2023-10-12 17:52:48 +0100

    builded for production

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+
 import VideoBackground from "./components/VideoBackground";
 import About from "./components/About";
 
